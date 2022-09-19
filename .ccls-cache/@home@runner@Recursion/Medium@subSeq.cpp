3ecb2475c7a8053df0b4424bf4fa0c6cf6114c16
Gopal Patel <42549416+gp20461@users.noreply.github.com>
2022-09-19 17:59:08 +0000

    initial commit

---
+++ .ccls-cache/@home@runner@Recursion/Medium@subSeq.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void fun(int arr[],vector<int>& vec,int n,int ind)
+{
+   if(ind == n)
+   {
+     for(auto it:vec)
+       {
+         cout << it << " ";
+       }
+     if(vec.size() == 0){
+       cout << "{}"<< " ";
+     }
+     cout << endl;
+     return;
+   }
+// pick element
+  vec.push_back(arr[ind]);
+  fun(arr,vec,n,ind+1);
+  vec.pop_back();
+
+// Don't pick element
+  fun(arr,vec,n,ind+1);
+}
+
+int main(){
+
+  int arr[] = {1,2,3};
+
+  int n = 3;
+
+  vector<int>vec;
+  
+  fun(arr,vec,n,0);
+  
+}
