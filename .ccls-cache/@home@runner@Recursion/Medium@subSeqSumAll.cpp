c09452759cd1a47a166e98a9267f5c3b32e1d8f5
Gopal Patel <42549416+gp20461@users.noreply.github.com>
2022-09-21 10:41:35 +0000

    initial commit

--- .ccls-cache/@home@runner@Recursion/Medium@subSeqSumAll.cpp
+++ .ccls-cache/@home@runner@Recursion/Medium@subSeqSumAll.cpp
@@ -3,27 +3,27 @@
 
 using namespace std;
 
-void fun(int arr[],vector<int>& vec,int n,int ind,int sum,int s)
+bool fun(int arr[],vector<int>& vec,int n,int ind,int sum,int s)
 {
    if(ind == n)
    {
      if(sum == s)
      {
         for(auto it:vec) cout << it << " ";
-        cout << endl;
+        return true;
      }
-   return;
+   return false;
    }
 // pick element
   vec.push_back(arr[ind]);
   s += arr[ind];
-  fun(arr,vec,n,ind+1,sum,s);
+  if(fun(arr,vec,n,ind+1,sum,s)) return true;
 
   s -= arr[ind];
   vec.pop_back();
 
 // Don't pick element
-  fun(arr,vec,n,ind+1,sum,s);
+  if(fun(arr,vec,n,ind+1,sum,s)) return true;
 }
 
 int main(){
@@ -32,7 +32,7 @@ int main(){
 
   int n = 3,ind = 0,s = 0;
 
-  int sum = 4;
+  int sum = 3;
   vector<int>vec;
   
   fun(arr,vec,n,ind,sum,s);
